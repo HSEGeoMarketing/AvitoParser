960b57e402df5b9feffe7723fe29c8a86d549a52
pagaf <112046185+pagaf@users.noreply.github.com>
2023-04-10 22:24:36 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,77 +1,103 @@
-import asyncio
-import re
-import aiohttp as aiohttp
-from bs4 import BeautifulSoup
-import asyncpg
-import random
-import time
+import sqlite3
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from math import ceil
+
+URL_AVITO = f"https://www.avito.ru/sankt-peterburg/kommercheskaya_nedvizhimost"
+EUR = 1
 
 
-async def fetch(url, headers=None, proxy=None):
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url, proxy=proxy) as response:
-            return await response.text()
+def avito_parser():
+    options = Options()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    # Задание user-agent
+    ua = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
+    options.add_argument(f"user-agent={ua}")
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(15)
+    driver.get(URL_AVITO)
+
+    count = int(
+        (
+            driver.find_element(
+                by=By.CSS_SELECTOR, value='span[data-marker="page-title/count"]'
+            ).text
+        ).replace(" ", "")
+    )
+
+    for _ in range(ceil(count / 50)):
+        offer = []
+        elems = driver.find_elements(
+            by=By.CSS_SELECTOR, value='div[data-marker="item"]'
+        )
+        for elem in elems:
+            try:
+                avito_id = int(elem.get_attribute("id")[1:])
+                url = elem.find_element(
+                    by=By.CSS_SELECTOR, value='a[itemprop="url"]'
+                ).get_attribute("href")
+                item_address = elem.find_element(
+                    by=By.CSS_SELECTOR, value='div[data-marker="item-address"]'
+                ).text.split("\n")
+                address = item_address[0]
+                district = item_address[1]
+                advert = elem.text.split("\n")
+                price = round(int(advert[1][:-10].replace(" ", "")), 2)
+                area = float(advert[0].split(", ")[1][:-3].replace(",", "."))
+                parking = "Да" if "Парковка" in advert else "Нет"
+                result = (area, price, address, parking, url)
+                offer.append(result)
+
+            except Exception as ex:
+                print(ex)
+
+        with sqlite3.connect("realty.db") as connection:
+            cursor = connection.cursor()
+            for data in offer:
+                avito_id = data[0]
+                cursor.execute(
+                    """
+                    SELECT avito_id FROM offers WHERE avito_id = (?)
+                """,
+                    (avito_id,),
+                )
+                result = cursor.fetchone()
+                if result is None:
+                    cursor.execute(
+                        """
+                        INSERT INTO offers
+                        VALUES (NULL, ?, ?, ?, ?, ?, ?)
+                    """,
+                        data,
+                    )
+                    connection.commit()
+                    print(f"Объявление {avito_id} добавлено в базу данных")
+                else:
+                    print(f"Объявление {avito_id} не добавлено в базу данных")
+        driver.find_element(
+            by=By.CSS_SELECTOR, value='span[data-marker="pagination-button/next"]'
+        ).click()
+
+    driver.quit()
 
 
-async def parse_page(url, conn):
-    # Имитация естественного поведения
-    delay = random.uniform(1, 3)  # Случайная задержка от 1 до 3 секунд
-    await asyncio.sleep(delay)
-
-    html = await fetch(url)
-    soup = BeautifulSoup(html, 'html.parser')
-    ads = soup.find_all('div', {'class': 'iva-item-root-Nj_hb'})
-    for ad in ads:
-        link = ad.find('a', {'class': 'link-link-39EVK'})
-        if link:
-            href = link.get('href')
-            url = f'https://www.avito.ru{href}'
-            price = ad.find('span', {'class': 'price-price-32bra'}).text.strip().replace(' ', '')
-            square = None
-            square_regex = r'(\d+\.\d+) м²'
-            square_match = re.search(square_regex, ad.text)
-            if square_match:
-                square = square_match.group(1)
-            address = ad.find('span', {'class': 'geo-address-9QndR'})
-            if address:
-                address = address.text.strip()
-            lat, lon = None, None
-            map_link = ad.find('a', {'class': 'item__map'})
-            if map_link:
-                map_url = map_link.get('href')
-                map_html = await fetch(map_url)
-                map_soup = BeautifulSoup(map_html, 'html.parser')
-                map_script = map_soup.find('script', {'type': 'application/json'})
-                if map_script:
-                    map_data = map_script.text
-                    lat_lon_regex = r'"coordinates":\[(\d+\.\d+),(\d+\.\d+)\]'
-                    lat_lon_match = re.search(lat_lon_regex, map_data)
-                    if lat_lon_match:
-                        lat = float(lat_lon_match.group(1))
-                        lon = float(lat_lon_match.group(2))
-            await conn.execute('''INSERT INTO COMMERCIAL_PREMISES 
-                                  (SQUARE, PRICE, ADDRESS, LATITUDE, LONGITUDE, LINK) 
-                                  VALUES ($1, $2, $3, $4, $5, $6)''', (square, price, address, lat, lon, url))
-
-
-async def main():
-    conn = await asyncpg.connect(database="db_avito", user="postgres", password="1111", host="localhost", port="5432")
-    await conn.execute('''CREATE TABLE IF NOT EXISTS COMMERCIAL_PREMISES (
-                            ID SERIAL PRIMARY KEY,
-                            SQUARE DOUBLE PRECISION,
-                            PRICE INT,
-                            ADDRESS TEXT,
-                            LATITUDE DOUBLE PRECISION,
-                            LONGITUDE DOUBLE PRECISION,
-LINK TEXT);''')
-    urls = ['https://www.avito.ru/moskva/kommercheskaya_nedvizhimost/prodam',
-            'https://www.avito.ru/moskva/kommercheskaya_nedvizhimost/prodam?p=2',
-            'https://www.avito.ru/moskva/kommercheskaya_nedvizhimost/prodam?p=3']
-
-    tasks = []
-    for url in urls:
-        tasks.append(parse_page(url, conn))
-
-    await asyncio.gather(*tasks)
-
-    await conn.close()
+if __name__ == "__main__":
+    connection = sqlite3.connect("realty.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        """ CREATE TABLE IF NOT EXISTS offers (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    avito_id INTEGER UNIQUE NOT NULL,
+                    area REAL NOT NULL,
+                    price REAL NOT NULL,
+                    address TEXT NOT NULL,
+                    url TEXT NOT NULL
+                    );
+    """
+    )
+    connection.close()
+    avito_parser()
