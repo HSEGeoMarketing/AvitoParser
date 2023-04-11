4a3454531f3b25f8a8b8f57e6dcfc21a392ae540
pagaf <112046185+pagaf@users.noreply.github.com>
2023-04-11 17:17:42 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,103 +1,51 @@
-import sqlite3
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+from geopy.geocoders import Nominatim
+from bs4 import BeautifulSoup
+import time
+import csv
 
-from math import ceil
+geolocator = Nominatim(user_agent="myGeocoder")
+with open('commercial_premises.csv', "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(('Name', 'Price', 'Address', 'Latitude', 'Longitude', 'Link'))
 
-URL_AVITO = f"https://www.avito.ru/sankt-peterburg/kommercheskaya_nedvizhimost"
-EUR = 1
+i = 1
 
-
-def avito_parser():
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    # Задание user-agent
-    ua = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
-    options.add_argument(f"user-agent={ua}")
-
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(15)
-    driver.get(URL_AVITO)
-
-    count = int(
-        (
-            driver.find_element(
-                by=By.CSS_SELECTOR, value='span[data-marker="page-title/count"]'
-            ).text
-        ).replace(" ", "")
+while i != 101:
+    URL = f'https://www.avito.ru/sankt-peterburg/kommercheskaya_nedvizhimost/sdam-ASgBAgICAUSwCNRW?cd={i}'
+    driver = webdriver.Chrome(
+        executable_path="C:\\Users\\Пользователь\\Desktop\\project\\parsing\\parser\\ChromeWebDriver\\chromedriver.exe"
     )
 
-    for _ in range(ceil(count / 50)):
-        offer = []
-        elems = driver.find_elements(
-            by=By.CSS_SELECTOR, value='div[data-marker="item"]'
-        )
-        for elem in elems:
-            try:
-                avito_id = int(elem.get_attribute("id")[1:])
-                url = elem.find_element(
-                    by=By.CSS_SELECTOR, value='a[itemprop="url"]'
-                ).get_attribute("href")
-                item_address = elem.find_element(
-                    by=By.CSS_SELECTOR, value='div[data-marker="item-address"]'
-                ).text.split("\n")
-                address = item_address[0]
-                district = item_address[1]
-                advert = elem.text.split("\n")
-                price = round(int(advert[1][:-10].replace(" ", "")), 2)
-                area = float(advert[0].split(", ")[1][:-3].replace(",", "."))
-                parking = "Да" if "Парковка" in advert else "Нет"
-                result = (area, price, address, parking, url)
-                offer.append(result)
+    try:
+        driver.get(url=URL)
+        main_page = driver.page_source
+        time.sleep(1)
+        main_source = BeautifulSoup(main_page, "html.parser")
+        adt = main_source.find_all('div', class_='iva-item-content-rejJg')
+        for item in adt:
+            title = item.find('h3', class_='title-root-zZCwT')
+            price = item.find('span', class_='price-text-_YGDY')
+            address = item.find('div', class_='geo-address-fhHd0')
+            location = geolocator.geocode(address.text.strip())
+            if location is not None:
+                latitude = location.latitude
+                longitude = location.longitude
+            else:
+                latitude = None
+                longitude = None
+            link = item.find('a', class_='link-link-MbQDP')
+            link = link.get('href')
+            link = 'https://www.avito.ru' + link
+            with open('commercial_premises.csv', "a", encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (title.text.strip(), price.text.strip(), address.text.strip(), latitude,
+                     longitude, link.strip()))
 
-            except Exception as ex:
-                print(ex)
-
-        with sqlite3.connect("realty.db") as connection:
-            cursor = connection.cursor()
-            for data in offer:
-                avito_id = data[0]
-                cursor.execute(
-                    """
-                    SELECT avito_id FROM offers WHERE avito_id = (?)
-                """,
-                    (avito_id,),
-                )
-                result = cursor.fetchone()
-                if result is None:
-                    cursor.execute(
-                        """
-                        INSERT INTO offers
-                        VALUES (NULL, ?, ?, ?, ?, ?, ?)
-                    """,
-                        data,
-                    )
-                    connection.commit()
-                    print(f"Объявление {avito_id} добавлено в базу данных")
-                else:
-                    print(f"Объявление {avito_id} не добавлено в базу данных")
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='span[data-marker="pagination-button/next"]'
-        ).click()
-
-    driver.quit()
-
-
-if __name__ == "__main__":
-    connection = sqlite3.connect("realty.db")
-    cursor = connection.cursor()
-    cursor.execute(
-        """ CREATE TABLE IF NOT EXISTS offers (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    avito_id INTEGER UNIQUE NOT NULL,
-                    area REAL NOT NULL,
-                    price REAL NOT NULL,
-                    address TEXT NOT NULL,
-                    url TEXT NOT NULL
-                    );
-    """
-    )
-    connection.close()
-    avito_parser()
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+    i += 1
