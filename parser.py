6c761339861be983aad6f08cd32662fd68cf019e
pagaf <112046185+pagaf@users.noreply.github.com>
2023-04-25 03:12:13 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import psycopg2 as psycopg2
+from sqlalchemy import create_engine
 from selenium import webdriver
 from geopy.geocoders import Nominatim
 from bs4 import BeautifulSoup
@@ -13,7 +13,7 @@ with open('commercial_premises.csv', "w", encoding="utf-8") as file:
     writer.writerow(('Name', 'Price', 'Square', 'Address', 'Latitude', 'Longitude', 'Link'))
 
 # Specify the number of pages to parse
-num_pages = 100
+num_pages = 1
 
 for i in range(1, num_pages + 1):
     URL = f'https://www.avito.ru/sankt-peterburg/kommercheskaya_nedvizhimost/sdam-ASgBAgICAUSwCNRW?cd={i}&p={i}'
@@ -29,6 +29,7 @@ for i in range(1, num_pages + 1):
         for item in adt:
             title = item.find('h3', class_='title-root-zZCwT')
             price = item.find('span', class_='price-text-_YGDY')
+            price_text = price.text.replace('\xa0', '') if price else None
             address = item.find('div', class_='geo-address-fhHd0')
             location = None
             if address:
@@ -49,9 +50,21 @@ for i in range(1, num_pages + 1):
             title_text = title.text.strip() if title else None
             match = re.search(regex, title_text) if title_text else None
             if match:
-                square = match.group(1)
+                square = float(match.group(1))
             else:
                 square = None
+            if 'за м²' in price_text:
+                price_match = re.search(r'\d+', price_text)
+                if price_match:
+                    price_value = float(price_match.group()) * square
+                else:
+                    price_value = None
+            else:
+                price_match = re.search(r'\d+', price_text)
+                if price_match:
+                    price_value = float(price_match.group())
+                else:
+                    price_value = None
             if location is not None:
                 latitude = location.latitude
                 longitude = location.longitude
@@ -61,63 +74,30 @@ for i in range(1, num_pages + 1):
             link = item.find('a', class_='link-link-MbQDP')
             link = link.get('href') if link else None
             link = 'https://www.avito.ru' + link if link else None
-            price_text = price.text.replace('\xa0', '') if price else None
-
             with open('commercial_premises.csv', "a", encoding='utf-8', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(
-                    (title.text.strip() if title else None, price_text, square,
+                    (title.text.strip() if title else None, price_value, square,
                      address_text if address else None, latitude, longitude, link.strip() if link else None))
 
     except Exception as ex:
         print(ex)
     finally:
         driver.quit()
+df = pd.read_csv('commercial_premises.csv')
+# удаляем строки с пропущенными значениями (NaN)
+df = df.dropna()
+# записываем измененный DataFrame обратно в csv файл
+df.to_csv('commercial_premises.csv', index=False)
+engine = create_engine("postgresql://")
 
-# Connect to PostgreSQL
-conn = psycopg2.connect(
-    dbname="geo_marketing",
-    user="student",
-    host="46.148.230.201",
-    password="jDdKxbj@=vYdsRHe-pyfMGvQ$^F6BQhrD@yfCKHHdS9WPxu^Sk&mGceFeJG9p*g=S^qz!CWDdB!SrRc7bYpjPdTd2+TF4g?k55+?",  # укажите ваш пароль
-    port="5432",  # указать порт, если отличается от порта по умолчанию (5432)
-)
 
-cursor = conn.cursor()
+data = pd.read_csv('commercial_premises.csv')
 
-# Create Table
-create_table = '''
-CREATE TABLE IF NOT EXISTS commercial_premises (
-    id SERIAL PRIMARY KEY,
-    Name TEXT,
-    Price TEXT,
-    Square REAL,
-    Address TEXT,
-    Latitude REAL,
-    Longitude REAL,
-    Link TEXT
-);
-'''
-cursor.execute(create_table)
-
-# Import CSV
-data = pd.read_csv(r'commercial_premises.csv')
-
-# Insert DataFrame to Table
-for _, row in data.iterrows():
-    cursor.execute('''
-        INSERT INTO commercial_premises (Name, Price, Square, Address, Latitude, Longitude, Link)
-        VALUES (%s, %s, %s, %s, %s, %s, %s)
-    ''', (
-        row['Name'],
-        row['Price'],
-        row['Square'],
-        row['Address'],
-        row['Latitude'],
-        row['Longitude'],
-        row['Link']
-    ))
-
-conn.commit()
-cursor.close()
-conn.close()
+with engine.connect() as conn, conn.begin():
+    data.to_sql(
+        'commercial_premises',
+        con=conn,
+        if_exists='append',
+        index=False
+    )
