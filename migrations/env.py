db80d431b2f709ea0b8df8078a6ab2febf29a76b
pagaf <112046185+pagaf@users.noreply.github.com>
2023-05-01 16:39:04 +0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,78 +1,52 @@
-from logging.config import fileConfig
+import csv
+from sqlalchemy import create_engine, Table, Column, Float, Integer, MetaData, String
+from alembic import op
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+# Подключение к базе данных
+engine = create_engine('postgresql://student:@46.148.230.201:5432/geo_marketing')
 
-from alembic import context
-import models
+# Определение структуры таблицы
+metadata = MetaData()
+commercial_premises = Table('commercial_premises', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String),
+    Column('price', Float),
+    Column('square', Float),
+    Column('address', String),
+    Column('latitude', Float),
+    Column('longitude', Float),
+    Column('link', String)
+)
 
+def upgrade():
+    op.execute("""
+        CREATE TABLE commercial_premises (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR NOT NULL,
+            price FLOAT NOT NULL,
+            square FLOAT NOT NULL,
+            address VARCHAR NOT NULL,
+            latitude FLOAT NOT NULL,
+            longitude FLOAT NOT NULL,
+            link VARCHAR NOT NULL
+        )
+    """)
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+    # Чтение данных из CSV файла и вставка их в таблицу
+    with open('path/to/commercial_premises.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # пропуск заголовочной строки
+        for row in reader:
+            stmt = commercial_premises.insert().values(
+                title=row[0],
+                price=float(row[1]),
+                square=float(row[2]),
+                address=row[3],
+                latitude=float(row[4]),
+                longitude=float(row[5]),
+                link=row[6]
+            )
+            engine.execute(stmt)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+def downgrade():
+    op.execute("DROP TABLE commercial_premises")
